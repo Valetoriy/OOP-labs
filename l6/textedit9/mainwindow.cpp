2c495328dc49ecc659a77dfcfa5ef90e4f2f8849
shadolproff <shadolproff420@mail.ru>
2022-09-15 00:51:55 +0300

    l6

---
+++ l6/textedit9/mainwindow.cpp
@@ -0,0 +1,70 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <algorithm>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_reverseL_triggered()
+{
+    QString str{ui->EdtLeft->text()};
+
+    std::reverse(str.begin(), str.end());
+    ui->EdtLeft->setText(str);
+}
+
+
+void MainWindow::on_reverseR_triggered()
+{
+    QString str{ui->EdtRight->text()};
+
+    std::reverse(str.begin(), str.end());
+    ui->EdtRight->setText(str);
+}
+
+
+void MainWindow::on_reverseAll_triggered()
+{
+    on_reverseL_triggered();
+    on_reverseR_triggered();
+}
+
+
+void MainWindow::on_swap_triggered()
+{
+    QString strL{ui->EdtLeft->text()};
+    QString strR{ui->EdtRight->text()};
+
+    ui->EdtLeft->setText(strR);
+    ui->EdtRight->setText(strL);
+}
+
+
+void MainWindow::on_concatLR_triggered()
+{
+    QString strL{ui->EdtLeft->text()};
+    QString strR{ui->EdtRight->text()};
+
+    ui->EdtLeft->setText(strL + strR);
+}
+
+
+void MainWindow::on_concatRL_triggered()
+{
+    QString strL{ui->EdtLeft->text()};
+    QString strR{ui->EdtRight->text()};
+
+    ui->EdtRight->setText(strR + strL);
+}
+
--- l6/textedit9/mainwindow.h
+++ l6/textedit9/mainwindow.h
@@ -15,6 +15,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_reverseL_triggered();
+
+    void on_reverseR_triggered();
+
+    void on_reverseAll_triggered();
+
+    void on_swap_triggered();
+
+    void on_concatLR_triggered();
+
+    void on_concatRL_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
